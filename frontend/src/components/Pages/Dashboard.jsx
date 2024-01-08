3e4ac907ad2ed4e662c0bfb8634ccfe20405a556
tranhuuloc <tranhuuloc4423@gmail.com>
2024-01-08 22:18:38 +0700

    add table

--- frontend/src/components/Pages/Dashboard.jsx
+++ frontend/src/components/Pages/Dashboard.jsx
@@ -1,5 +1,14 @@
+import Table from '../Table'
+
 const Dashboard = () => {
-    return <div>Dashboard</div>
+    return (
+        <div className="grid grid-cols-2 grid-rows-2 w-full gap-10 mt-5">
+            <Table title={'Room state'} />
+            <Table title={'Revenue'} />
+            <Table title={'List of available rooms'} />
+            <Table title={'Customer list lacks money'} />
+        </div>
+    )
 }
 
 export default Dashboard
