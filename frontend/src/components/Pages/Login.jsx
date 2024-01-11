b7f3e99c29ae5019aeb4d83de59749b360605cd5
tranhuuloc <tranhuuloc4423@gmail.com>
2024-01-11 22:31:18 +0700

    add sign in/up form

--- frontend/src/components/Pages/Login.jsx
+++ frontend/src/components/Pages/Login.jsx
@@ -1,17 +1,5 @@
 import { useState } from 'react'
-import {
-    MDBRow,
-    MDBCol,
-    MDBInput,
-    MDBCheckbox,
-    MDBBtn,
-    MDBIcon,
-    MDBTabs,
-    MDBTabsItem,
-    MDBTabsLink,
-    MDBTabsContent,
-    MDBTabsPane
-} from 'mdb-react-ui-kit'
+import { MDBInput, MDBCol, MDBRow, MDBCheckbox, MDBBtn, MDBIcon } from 'mdb-react-ui-kit'
 
 const Login = () => {
     const [loginRegisterActive, setLoginRegisterActive] = useState('register')
@@ -21,118 +9,46 @@ const Login = () => {
         setLoginRegisterActive(value)
     }
     return (
-        <div className="w-[500px]">
-            <MDBTabs pills justify className="mb-3">
-                <MDBTabsItem>
-                    <MDBTabsLink
-                        onClick={() => handleLoginRegisterClick('login')}
-                        active={loginRegisterActive === 'login'}
-                    >
-                        Login
-                    </MDBTabsLink>
-                </MDBTabsItem>
-                <MDBTabsItem>
-                    <MDBTabsLink
-                        onClick={() => handleLoginRegisterClick('register')}
-                        active={loginRegisterActive === 'register'}
-                    >
-                        Register
-                    </MDBTabsLink>
-                </MDBTabsItem>
-            </MDBTabs>
+        <form className="w-[380px] absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+            <MDBInput className="mb-4" type="email" id="form2Example1" label="Email" />
+            <MDBInput className="mb-4" type="password" id="form2Example2" label="Password" />
 
-            <MDBTabsContent>
-                <MDBTabsPane show={loginRegisterActive === 'login'}>
-                    <form>
-                        <div className="text-center mb-3">
-                            <p>Sign up with:</p>
+            <MDBRow className="mb-4">
+                <MDBCol className="d-flex justify-content-center select-none">
+                    <MDBCheckbox id="form2Example3" label="Remember me" defaultChecked />
+                </MDBCol>
+                <MDBCol>
+                    <a href="#!">Forgot password?</a>
+                </MDBCol>
+            </MDBRow>
 
-                            <MDBBtn floating color="secondary" className="mx-1">
-                                <MDBIcon fab icon="facebook-f" />
-                            </MDBBtn>
+            <MDBBtn type="submit" className="mb-4" block>
+                Sign in
+            </MDBBtn>
 
-                            <MDBBtn floating color="secondary" className="mx-1">
-                                <MDBIcon fab icon="google" />
-                            </MDBBtn>
+            <div className="text-center">
+                <p>
+                    Not a member? <a href="#!">Register</a>
+                </p>
+                <p>or sign up with:</p>
 
-                            <MDBBtn floating color="secondary" className="mx-1">
-                                <MDBIcon fab icon="twitter" />
-                            </MDBBtn>
+                <MDBBtn floating color="secondary" className="mx-1">
+                    <MDBIcon fab icon="facebook-f" />
+                </MDBBtn>
 
-                            <MDBBtn floating color="secondary" className="mx-1">
-                                <MDBIcon fab icon="github" />
-                            </MDBBtn>
-                        </div>
+                <MDBBtn floating color="secondary" className="mx-1">
+                    <MDBIcon fab icon="google" />
+                </MDBBtn>
 
-                        <p className="text-center">or:</p>
+                <MDBBtn floating color="secondary" className="mx-1">
+                    <MDBIcon fab icon="twitter" />
+                </MDBBtn>
 
-                        <MDBInput className="mb-4" type="email" id="form7Example1" label="Email address" />
-                        <MDBInput className="mb-4" type="password" id="form7Example2" label="Password" />
-
-                        <MDBRow className="mb-4">
-                            <MDBCol className="d-flex justify-content-center">
-                                <MDBCheckbox id="form7Example3" label="Remember me" defaultChecked />
-                            </MDBCol>
-                            <MDBCol>
-                                <a href="#!">Forgot password?</a>
-                            </MDBCol>
-                        </MDBRow>
-
-                        <MDBBtn type="submit" className="mb-4" block>
-                            Sign in
-                        </MDBBtn>
-
-                        <div className="text-center">
-                            <p>
-                                Not a member? <a href="#!">Register</a>
-                            </p>
-                        </div>
-                    </form>
-                </MDBTabsPane>
-                <MDBTabsPane show={loginRegisterActive === 'register'}>
-                    <form>
-                        <div className="text-center mb-3">
-                            <p>Sign up with:</p>
-
-                            <MDBBtn floating color="secondary" className="mx-1">
-                                <MDBIcon fab icon="facebook-f" />
-                            </MDBBtn>
-
-                            <MDBBtn floating color="secondary" className="mx-1">
-                                <MDBIcon fab icon="google" />
-                            </MDBBtn>
-
-                            <MDBBtn floating color="secondary" className="mx-1">
-                                <MDBIcon fab icon="twitter" />
-                            </MDBBtn>
-
-                            <MDBBtn floating color="secondary" className="mx-1">
-                                <MDBIcon fab icon="github" />
-                            </MDBBtn>
-                        </div>
-
-                        <p className="text-center">or:</p>
-
-                        <MDBInput className="mb-4" id="form8Example1" label="Name" />
-                        <MDBInput className="mb-4" id="form8Example2" label="Username" />
-                        <MDBInput className="mb-4" type="email" id="form8Example3" label="Email address" />
-                        <MDBInput className="mb-4" type="password" id="form8Example4" label="Password" />
-                        <MDBInput className="mb-4" type="password" id="form8Example5" label="Repeat password" />
-
-                        <MDBCheckbox
-                            wrapperClass="d-flex justify-content-center mb-4"
-                            id="form8Example6"
-                            label="I have read and agree to the terms"
-                            defaultChecked
-                        />
-
-                        <MDBBtn type="submit" className="mb-4" block>
-                            Sign in
-                        </MDBBtn>
-                    </form>
-                </MDBTabsPane>
-            </MDBTabsContent>
-        </div>
+                <MDBBtn floating color="secondary" className="mx-1">
+                    <MDBIcon fab icon="github" />
+                </MDBBtn>
+            </div>
+        </form>
     )
 }
 
