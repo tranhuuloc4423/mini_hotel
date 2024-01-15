7ff4fac55e13d7fe7d60c252992ebc86e5c4bdce
tranhuuloc <tranhuuloc4423@gmail.com>
2024-01-15 22:09:30 +0700

    update report page

--- frontend/src/components/Pages/Report.jsx
+++ frontend/src/components/Pages/Report.jsx
@@ -12,6 +12,8 @@ import {
     MDBDropdownItem
 } from 'mdb-react-ui-kit'
 import icons from '../../utils/icons'
+import Chart from '../Chart'
+import Table from '../Table'
 
 const { IoAddCircleOutline } = icons
 const Report = () => {
@@ -29,36 +31,24 @@ const Report = () => {
         <div className="main-container">
             <div className="main-header">
                 <MDBTabs className="flex-center-y gap-2">
-                    <MDBTabsItem className="">
+                    <MDBTabsItem>
                         <MDBTabsLink onClick={() => handleBasicClick('tab1')} active={basicActive === 'tab1'}>
+                            Revenue
+                        </MDBTabsLink>
+                    </MDBTabsItem>
+                    <MDBTabsItem>
+                        <MDBTabsLink onClick={() => handleBasicClick('tab2')} active={basicActive === 'tab2'}>
+                            Bill list
+                        </MDBTabsLink>
+                    </MDBTabsItem>
+                    <MDBTabsItem>
+                        <MDBTabsLink onClick={() => handleBasicClick('tab3')} active={basicActive === 'tab3'}>
                             Paid list
                         </MDBTabsLink>
                     </MDBTabsItem>
-                    <MDBTabsItem className="p-0">
-                        <MDBTabsLink
-                            onClick={() => handleBasicClick('tab2')}
-                            active={basicActive === 'tab2'}
-                            className="px-6 py-3 m-0"
-                        >
-                            Paid list
-                        </MDBTabsLink>
-                    </MDBTabsItem>
-                    <MDBTabsItem className="p-0">
-                        <MDBTabsLink
-                            onClick={() => handleBasicClick('tab3')}
-                            active={basicActive === 'tab3'}
-                            className="px-6 py-3 m-0"
-                        >
-                            Paid list
-                        </MDBTabsLink>
-                    </MDBTabsItem>
-                    <MDBTabsItem className="p-0">
-                        <MDBTabsLink
-                            onClick={() => handleBasicClick('tab4')}
-                            active={basicActive === 'tab4'}
-                            className="px-6 py-3 m-0"
-                        >
-                            Paid list
+                    <MDBTabsItem>
+                        <MDBTabsLink onClick={() => handleBasicClick('tab4')} active={basicActive === 'tab4'}>
+                            Lack money list
                         </MDBTabsLink>
                     </MDBTabsItem>
                 </MDBTabs>
@@ -83,10 +73,18 @@ const Report = () => {
             </div>
             <div className="main-body">
                 <MDBTabsContent>
-                    <MDBTabsPane open={basicActive === 'tab1'}>Paid list</MDBTabsPane>
-                    <MDBTabsPane open={basicActive === 'tab2'}>lack money list</MDBTabsPane>
-                    <MDBTabsPane open={basicActive === 'tab3'}>Revenue</MDBTabsPane>
-                    <MDBTabsPane open={basicActive === 'tab4'}>bill list</MDBTabsPane>
+                    <MDBTabsPane open={basicActive === 'tab1'}>
+                        <Chart />
+                    </MDBTabsPane>
+                    <MDBTabsPane open={basicActive === 'tab2'}>
+                        <Table />
+                    </MDBTabsPane>
+                    <MDBTabsPane open={basicActive === 'tab3'}>
+                        <Table />
+                    </MDBTabsPane>
+                    <MDBTabsPane open={basicActive === 'tab4'}>
+                        <Table />
+                    </MDBTabsPane>
                 </MDBTabsContent>
             </div>
         </div>
