3ab304551ba16fa47a6d8ab14308fd72cf1834d4
LeVanTuanCoder <vantuanmhung2001@gmail.com>
2024-03-10 12:48:42 +0700

    fix annual data

--- backend/controllers/annualdataControllers.js
+++ backend/controllers/annualdataControllers.js
@@ -1,62 +1,61 @@
 const AnnualData = require('../models/AnnualData')
 const Invoice = require('../models/Invoice');
 
-const createMonthlyData = async (req, res) => {
-    try {
-        const { month } = req.params;
-        const paidInvoices = await Invoice.find({ time: month, status: 'paid' });
-        const revenue = paidInvoices.reduce((totalRevenue, invoice) => totalRevenue + invoice.total, 0);
-        const monthlyData = {
-            month,
-            revenue,
-            invoices: paidInvoices
-        };
+const annualDataController =  {
+    createMonthlyData : async (req, res) => {
+        try {
+            const { month } = req.params;
+            const paidInvoices = await Invoice.find({ time: month, status: 'paid' });
+            const revenue = paidInvoices.reduce((totalRevenue, invoice) => totalRevenue + invoice.total, 0);
+            const monthlyData = {
+                month,
+                revenue,
+                invoices: paidInvoices
+            };
+        
+            let annualData = await AnnualData.findOne();
+            if (!annualData) {
+              annualData = new AnnualData({
+                data: [monthlyData]
+              });
+              await annualData.save();
+              return res.status(201).json(annualData);
+            }
     
-        let annualData = await AnnualData.findOne();
-        if (!annualData) {
-          annualData = new AnnualData({
-            data: [monthlyData]
-          });
-          await annualData.save();
-          return res.status(201).json(annualData);
+            const existingMonthData = annualData.data.find(data => data.month === month);
+            if (existingMonthData) {
+              return res.status(400).json({ error: 'Dữ liệu theo tháng đã tồn tại' });
+            }
+    
+            annualData.data.push(monthlyData);
+            await annualData.save();
+            return res.status(201).json(annualData);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Lỗi server' });
         }
-
-        const existingMonthData = annualData.data.find(data => data.month === month);
-        if (existingMonthData) {
-          return res.status(400).json({ error: 'Dữ liệu theo tháng đã tồn tại' });
+    },
+    
+    getTotalRevenueByMonth: async (req, res) => {
+        try {
+            const { month } = req.params;
+            const annualData = await AnnualData.findOne();
+            if (!annualData) {
+            return res.status(404).json({ error: 'Không tìm thấy dữ liệu theo năm' });
+            }
+            const monthlyData = annualData.data.find(data => data.month === month);
+            if (!monthlyData) {
+            return res.status(404).json({ error: 'Không tìm thấy dữ liệu theo tháng' });
+            }
+            return res.status(200).json(monthlyData);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Lỗi server' });
         }
-
-        annualData.data.push(monthlyData);
-        await annualData.save();
-        return res.status(201).json(annualData);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Lỗi server' });
-    }
+    } 
 };
 
-const getTotalRevenueByMonth = async (req, res) => {
-    try {
-        const { month } = req.params;
-        const annualData = await AnnualData.findOne();
-        if (!annualData) {
-        return res.status(404).json({ error: 'Không tìm thấy dữ liệu theo năm' });
-        }
-        const monthlyData = annualData.data.find(data => data.month === month);
-        if (!monthlyData) {
-        return res.status(404).json({ error: 'Không tìm thấy dữ liệu theo tháng' });
-        }
-        return res.status(200).json(monthlyData);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Lỗi server' });
-    }
-};
-
-module.exports = {
-    createMonthlyData,
-    getTotalRevenueByMonth
-}
+module.exports = annualDataController
 
 // const data = {
 //     january: {
--- backend/routes/annualdata.js
+++ backend/routes/annualdata.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const annualDataController = require('../controllers/annualdataControllers.');
+const annualDataController = require('../controllers/annualdataControllers');
 
 router.post('/data/:month', annualDataController.createMonthlyData);
 router.get('/data/:month', annualDataController.getTotalRevenueByMonth);
